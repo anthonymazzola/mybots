d48389d6d73b9c826495cf5add4693234f668ce0
anthonymazzola <112509417+anthonymazzola@users.noreply.github.com>
2023-01-30 17:16:58 -0500

    Created Create_World Function.

--- generate.py
+++ generate.py
@@ -8,25 +8,9 @@ x = 0.0
 y= 0.0
 z = 0.5
 
-i = 0
-j = 0
-k = 0
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])    
+    pyrosim.End()
 
-pyrosim.Start_SDF("boxes.sdf")
-while k < 5:
-    j = 0
-    while j < 5:
-        i = 0
-        length = 1
-        width = 1
-        height = 1
-        while i < 10:
-            pyrosim.Send_Cube(name="Box", pos=[x+j,y+k,z+i] , size=[length,width,height])
-            i += 1
-            length *= .9
-            width *= .9
-            height *= .9
-        j += 1
-    k += 1
-    
-pyrosim.End()
+Create_World()
