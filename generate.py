3d49dd43c2b67d4f6091f26959cf810691d8b1ce
anthonymazzola <112509417+anthonymazzola@users.noreply.github.com>
2023-01-30 12:50:14 -0500

    Generating towers using nested loops.

--- generate.py
+++ generate.py
@@ -1,13 +1,32 @@
 import pyrosim.pyrosim as pyrosim
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
 x = 0.0
 y= 0.0
-z = 1.5
+z = 0.5
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+i = 0
+j = 0
+k = 0
+
+pyrosim.Start_SDF("boxes.sdf")
+while k < 5:
+    j = 0
+    while j < 5:
+        i = 0
+        length = 1
+        width = 1
+        height = 1
+        while i < 10:
+            pyrosim.Send_Cube(name="Box", pos=[x+j,y+k,z+i] , size=[length,width,height])
+            i += 1
+            length *= .9
+            width *= .9
+            height *= .9
+        j += 1
+    k += 1
+    
 pyrosim.End()
