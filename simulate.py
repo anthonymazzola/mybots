51bfb553923ad41025d7dbc7cd67f631d4a5ffc7
anthonymazzola <112509417+anthonymazzola@users.noreply.github.com>
2023-02-06 11:27:23 -0500

    Added numpy, created a vector of 0s, stored sensor values in vector.

--- simulate.py
+++ simulate.py
@@ -1,14 +1,22 @@
 import pybullet as p
+import numpy
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
+
+pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(1000)
+print(backLegSensorValues)
+
 for x in range(1000):
     p.stepSimulation()
+    backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
     time.sleep(1/60)
-    print(x)
 p.disconnect()
