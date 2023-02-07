08f1aa492810b486c660f6bd805a8c1d412091e7
anthonymazzola <112509417+anthonymazzola@users.noreply.github.com>
2023-02-07 09:55:02 -0500

    Getting the sensor values and using Numpy to save them into a file.

--- simulate.py
+++ simulate.py
@@ -13,10 +13,14 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(1000)
-print(backLegSensorValues)
+frontLegSensorValues = numpy.zeros(1000)
+
 
 for x in range(1000):
     p.stepSimulation()
     backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
+    frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
     time.sleep(1/60)
+numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
+numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
 p.disconnect()
