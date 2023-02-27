992a32026ca6a2e2787fb77c7e3704e90fd0bb99
anthonymazzola <112509417+anthonymazzola@users.noreply.github.com>
2023-02-27 11:46:40 -0500

    Added generate brain and generate body function.

--- generate.py
+++ generate.py
@@ -13,7 +13,10 @@ def Create_World():
     pyrosim.Send_Cube(name="Box", pos=[3,3,z] , size=[length,width,height])    
     pyrosim.End()
     
-def Create_Robot():
+##def Create_Robot():
+    
+    
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
     pyrosim.Send_Joint( name = "Torso_Backleg" , parent= "Torso" , child = "Backleg" , type = "revolute", position = [-0.5,0.0,1])
@@ -21,6 +24,13 @@ def Create_Robot():
     pyrosim.Send_Joint( name = "Torso_Frontleg" , parent= "Torso" , child = "Frontleg" , type = "revolute", position = [0.5,0,1.0])
     pyrosim.Send_Cube(name="Frontleg", pos=[0.5,0,-0.5] , size=[length,width,height])
     pyrosim.End()
+    
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+    pyrosim.End()
 
 Create_World()
-Create_Robot()
+#Create_Robot()
+Generate_Body()
+Generate_Brain()
